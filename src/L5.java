1090f8ce0d43e5611ddb49f98808614910740d8d
richawes001 <richawes001@warren.k12.in.us>
2021-08-09 12:11:43 -0400

    Completed L3, L4, and L5 Notes.

--- src/L5.java
+++ src/L5.java
@@ -1,2 +1,35 @@
-public class L5 {
+public class L5{
+    public static void main(String[] args){
+
+        /*double basePay = 7.25;
+        double hoursWeek1 = 16.5;
+        double hoursWeek2 = 23.75;
+
+        double totalHours = hoursWeek1 + hoursWeek2;
+        double wages = basePay * totalHours;
+
+        System.out.println("Your bi-weekly paycheck is: $" + wages);*/
+
+        double fraction = 1/2.0;
+        System.out.println(fraction);
+
+        //Modulus is the REMAINDER after division
+
+        //30 % 6 = 0
+        //31 % 6 = 1
+        //32 % 6 = 2
+        //121 % 100 = 21
+        //45 % 8 = 5
+        //22 % 2 = 0
+        //23 % 2 = 1
+        //24 % 2 = 0
+
+        // 5 + 2 * 4 | 5 + 8 | 13
+        // 12 / 2 - 4 | 6 - 4 | 2
+        // 4 + 17 % 2 - 1 | 4 + 1 + 1 | 6   wrong (answer 4)
+        // 4 + 5 * 2 / 2 + 1 | 4 + 10 / 2 + 1 | 4 + 5 + 1 | 10
+        // 4 * (6 + 3 * 2) + 7 | 4 * (6 + 6) + 7 | 4 * (12) + 7 | 48 + 7 | 55
+
+
+    }
 }
