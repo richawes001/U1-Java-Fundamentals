8ec655faf8e23f7017d55c90a8898238c571974f
richawes001 <richawes001@warren.k12.in.us>
2021-08-13 12:05:15 -0400

    Completed 1.5 Practice Problems and Notes

--- 1.5 Practice Problems/src/CalculatingDistance.java
+++ 1.5 Practice Problems/src/CalculatingDistance.java
@@ -3,22 +3,7 @@ public class CalculatingDistance {
     /*
     Create a program that calculates a vehicle's distance based on speed traveled and time traveled.
 
-    Example: 55 mph for 45 minutes -> 41.25 miles   |   55/60 * 45 = 41.25
+    Example: 55 mph for 45 minutes -> 41.25 miles
 
     */
-
-    public static void main(String[] args) {
-
-        double miles = 88.0;
-        double minutes = 48.0;
-        double milesPerMinute = miles / minutes;
-
-        double minutesDriven = 272.0;
-
-        double milesDriven = milesPerMinute * minutesDriven;
-
-        System.out.print("You drove " + milesDriven + " miles in " + minutesDriven + " minutes.");
-
-    }
-
 }
--- Finding Averages/src/Averages.java
+++ Finding Averages/src/Averages.java
@@ -5,5 +5,16 @@ public class Averages {
     Example: 12, 13, 14, 15 -> 13.5
      */
 
+    public static void main(String[] args) {
 
+        int testScores1 = 1235;
+        int testScores2 = 13;
+        int testScores3 = -9523;
+        int testScores4 = 6594;
+
+        int sumTotal = testScores1 + testScores2 + testScores3 + testScores4;
+        double average = sumTotal/4.0;
+
+        System.out.println("The average is " + average);
+    }
 }
